0cf80725bd88427c80734bb81c23052dab0484e6
DavidWalshe93 <david.walshe93@gmail.com>
2020-09-12 19:31:00 +0100

    Scraped main page of listings.

--- craigslist/craigslist/items.py
+++ craigslist/craigslist/items.py
@@ -11,5 +11,6 @@ import scrapy
 class CraigslistItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    date = scrapy.Field()
     title = scrapy.Field()
     url = scrapy.Field()
--- craigslist/craigslist/spiders/jobs.py
+++ craigslist/craigslist/spiders/jobs.py
@@ -7,30 +7,33 @@ from craigslist.items import CraigslistItem
 
 class JobsSpider(scrapy.Spider):
     name = 'jobs'
-    allowed_domains = ['newyork.craigslist.org/']
-    start_urls = ['http://newyork.craigslist.org/search/egr']
+    allowed_domains = ['newyork.craigslist.org']
+    start_urls = ['https://newyork.craigslist.org/search/ofc/']
 
-    def parse(self, response):
+    def parse(self, response: scrapy.http.Response):
         """
         Parses content from a html page response.
 
         :param response: The page response/
         :return: The parsed payload.
         """
-        # loader = ItemLoader(item=JobsSpider(), response=response)
+        listings = response.xpath('//li[@class="result-row"]')
 
-        listing = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
-        urls = response.xpath('//a[@class="result-title hdrlnk"]/@href').extract()
+        for listing in listings:
+            loader = ItemLoader(item=CraigslistItem(), response=response)
+            # Relative matching
+            date = listing.xpath('.//*[@class="result-date"]/@datetime').extract_first()
+            url = listing.xpath('.//a[@class="result-title hdrlnk"]/@href').extract_first()
+            title = listing.xpath('.//a[@class="result-title hdrlnk"]/text()').extract_first()
 
-        for listing, url in zip(listing, urls):
-            yield dict(
-                listing=listing,
-                url=url
-            )
+            loader.add_value("date", date)
+            loader.add_value("title", title)
+            loader.add_value("url", url)
 
-        # loader.add_value("title", title)
-        # loader.add_value("url", url)
-        #
-        # return loader.load_item()
+            yield loader.load_item()
 
+        next_page_url = response.xpath('//*[@class="button next"]/@href').extract_first()
 
+        if next_page_url:
+            abs_next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(url=abs_next_page_url, callback=self.parse)

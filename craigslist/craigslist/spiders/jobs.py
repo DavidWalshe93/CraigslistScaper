20ea3aa46af1f97e55d593636db3872d8012a0ff
DavidWalshe93 <david.walshe93@gmail.com>
2020-09-12 14:37:07 +0100

    Obtained listing and listing URLs

--- craigslist/craigslist/spiders/jobs.py
+++ craigslist/craigslist/spiders/jobs.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+from scrapy.loader import ItemLoader
+from craigslist.items import CraigslistItem
+
 
 class JobsSpider(scrapy.Spider):
     name = 'jobs'
@@ -8,4 +11,26 @@ class JobsSpider(scrapy.Spider):
     start_urls = ['http://newyork.craigslist.org/search/egr']
 
     def parse(self, response):
-        pass
+        """
+        Parses content from a html page response.
+
+        :param response: The page response/
+        :return: The parsed payload.
+        """
+        # loader = ItemLoader(item=JobsSpider(), response=response)
+
+        listing = response.xpath('//a[@class="result-title hdrlnk"]/text()').extract()
+        urls = response.xpath('//a[@class="result-title hdrlnk"]/@href').extract()
+
+        for listing, url in zip(listing, urls):
+            yield dict(
+                listing=listing,
+                url=url
+            )
+
+        # loader.add_value("title", title)
+        # loader.add_value("url", url)
+        #
+        # return loader.load_item()
+
+
